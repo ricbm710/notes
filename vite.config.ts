09bdd9b321d1277fc27fa4f9c5a080e39138763a
Ricardo <ricardobarmont@gmail.com>
2024-06-12 09:13:03 -0400

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  return {
+    base: command === "build" ? "/notes/" : "/",
+    plugins: [react()],
+  };
+});
